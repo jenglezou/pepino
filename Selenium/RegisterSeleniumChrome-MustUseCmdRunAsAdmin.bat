30b4915f57f14a11874f8014656648ee02ba6ca1
j e <j e>
2016-01-15 15:43:36 +0000

    selenium dll and chromedriver

---
+++ Selenium/RegisterSeleniumChrome-MustUseCmdRunAsAdmin.bat
@@ -0,0 +1,2 @@
+REM *****************   This must be run in a command window started as ADMINISTRATOR *****************************
+c:\windows\microsoft.net\framework\v2.0.50727\regasm.exe Selenium.dll /tlb:selenium.tlb 
